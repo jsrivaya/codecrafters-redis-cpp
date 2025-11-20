be326707f2e5133ad7d727a0ee35b54d5e5a87fe
Jorge Suarez Rivaya <jsrivaya@gmail.com>
2025-11-20 09:46:16 -0800

    Add binding to a port support

--- src/main.cpp
+++ src/main.cpp
@@ -46,16 +46,11 @@ int main(int argc, char **argv) {
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
   std::cout << "Waiting for a client to connect...\n";
-
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  std::cout << "Logs from your program will appear here!\n";
-
-  // Uncomment the code below to pass the first stage
-  // 
-  // accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  // std::cout << "Client connected\n";
-  // 
-  // close(server_fd);
+  
+  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  std::cout << "Client connected\n";
+  
+  close(server_fd);
 
   return 0;
 }
